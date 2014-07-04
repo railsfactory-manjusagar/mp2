aea96b07aa2499dc45314fa1bf440c5c29fc2b0d
Manju Sagar <manjusagar@railsfactory.org>
2014-07-04 17:33:38 +0530

    test cases below 1000 are executed

--- spell_digit/lib/spell_digit.rb
+++ spell_digit/lib/spell_digit.rb
@@ -4,7 +4,8 @@ class Spell_digit
 def self.convert(number)
 
 
- hash = { 1=>'one',2=>'two',3=>'three', 4=>'four', 5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten'}
+ hash = { 1=>'one',2=>'two',3=>'three', 4=>'four', 5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten',100=>'hundred',200=>'twohundred',300=>'threehundred',400=>'fourhundred',
+500=>'fivehundred',600=>'sixhundred',700=>'sevenhundred',800=>'eighthundred',900=>'ninehundred',1000=>'thousand'}
  
   
 
@@ -16,6 +17,13 @@ def self.convert(number)
      number2 = number%10
    str = hash[number1]+"ty"+" "+hash[number2]
     return str
+   elsif number > 100 && number < 1000
+      number1 = number/100
+      number2 = number - number1*100
+      number3 = number2/10
+      number4 = number2%10
+      str = hash[number1]+"hundred"+" "+hash[number3]+"ty"+" "+hash[number4]
+      return str
     end
  
 
--- spell_digit/test/spell_digit_test.rb
+++ spell_digit/test/spell_digit_test.rb
@@ -10,6 +10,12 @@ class TestSpell_digit < Test::Unit::TestCase
   def test_one
    assert_equal "one",Spell_digit.convert(1)
    assert_equal "five",Spell_digit.convert(5)
-   assert_equal "seventy eight",Spell_digit.convert(78)
+   assert_equal "hundred",Spell_digit.convert(100)
+   assert_equal "hundred",Spell_digit.convert(100)
+   assert_equal "twohundred",Spell_digit.convert(200)
+   assert_equal "twohundred fourty nine",Spell_digit.convert(249)
+   assert_equal "sixhundred eightty nine",Spell_digit.convert(689)
+   assert_equal "sevenhundred eightty nine",Spell_digit.convert(789)
+   
   end
 end
