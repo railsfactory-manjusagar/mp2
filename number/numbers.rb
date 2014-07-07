896e1d1a258b108b07b5d219d5bb8541efc81d67
Manju Sagar <manjusagar@railsfactory.org>
2014-07-07 13:02:52 +0530

    to add the numbers with suffix for first four test cases

--- number/numbers.rb
+++ number/numbers.rb
@@ -6,7 +6,10 @@ class Numbers
       return "#{num}st"
     elsif num == 2
       return "#{num}nd"
-   
+    elsif num == 3
+      return "#{num}rd"
+    elsif num == 4 || num == 0
+      return "#{num}th"
   end
   
   end
