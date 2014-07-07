b652f62fd4784d370ef3ecf4e7c185c53b6409f1
Manju Sagar <manjusagar@railsfactory.org>
2014-07-07 10:49:17 +0530

    spell_digit till ten thousand with test cases

--- spell_digit/lib/spell_digit.rb
+++ spell_digit/lib/spell_digit.rb
@@ -1,35 +1,41 @@
 class Spell_digit
-
-
-def self.convert(number)
-
-
- hash = { 1=>'one',2=>'two',3=>'three', 4=>'four', 5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten',100=>'hundred',200=>'twohundred',300=>'threehundred',400=>'fourhundred',
-500=>'fivehundred',600=>'sixhundred',700=>'sevenhundred',800=>'eighthundred',900=>'ninehundred',1000=>'thousand'}
- 
-  
-
+ def self.convert(number)
+   hash = { 0=>'',1=>'one',2=>'two',3=>'three', 4=>'four',     5=>'five',6=>'six',7=>'seven',8=>'eight',9=>'nine',10=>'ten',100=>'hundred',200=>'twohundred',300=>'threehundred',400=>'fourhundred',
+500=>'fivehundred',600=>'sixhundred',700=>'sevenhundred',800=>'eighthundred',900=>'ninehundred',1000=>'thousand',11=>'eleven',12=>'twelve',
+13=>'thirteen',14=>'fourteen',15=>'fifteen',16=>'sixteen',17=>'seventeen',18=>'eighteen',19=>'ninteen',20=>'twenty'}
+     str = ""
   if  num = hash[number]
-       str =""
-       return num  
-   elsif number > 0 && number <100
-     number1 = number/10
-     number2 = number%10
-   str = hash[number1]+"ty"+" "+hash[number2]
-    return str
-   elsif number > 100 && number < 1000
+      return num  
+  elsif number > 0 && number <100
+      number1 = number/10
+      number2 = number%10
+      str = hash[number1]+"ty"+" "+hash[number2]
+      return str
+  elsif number > 100 && number <1000 
       number1 = number/100
       number2 = number - number1*100
       number3 = number2/10
       number4 = number2%10
+         if number2 >=11 && number2 <= 20
+         str = hash[number1]+"hundredand"+" "+hash[number2]
+         return str
+         end
       str = hash[number1]+"hundred"+" "+hash[number3]+"ty"+" "+hash[number4]
+      return str   
+
+  elsif number > 1000  
+      number0 = number/1000      
+      number5 = number - 1000 * number0
+      number6 = number5/100
+      number7 = number5 - 100 * number6
+      number8 = number7/10
+      number9 = number7%10
+         if number7 >=11 && number7 <=20
+         str = hash[number0]+"thousand"+" "+hash[number6]+"hundrend"+" "+hash[number7]
+         return str
+         end
+      str = hash[number0]+"thousand"+" "+hash[number6]+"hundrend"+" "+hash[number8]+"ty"+" "+hash[number9]
       return str
-    end
- 
-
- 
-
-
-end
-
+  end
+ end
 end
--- spell_digit/test/spell_digit_test.rb
+++ spell_digit/test/spell_digit_test.rb
@@ -11,11 +11,17 @@ class TestSpell_digit < Test::Unit::TestCase
    assert_equal "one",Spell_digit.convert(1)
    assert_equal "five",Spell_digit.convert(5)
    assert_equal "hundred",Spell_digit.convert(100)
-   assert_equal "hundred",Spell_digit.convert(100)
    assert_equal "twohundred",Spell_digit.convert(200)
    assert_equal "twohundred fourty nine",Spell_digit.convert(249)
    assert_equal "sixhundred eightty nine",Spell_digit.convert(689)
    assert_equal "sevenhundred eightty nine",Spell_digit.convert(789)
+   assert_equal "onehundredand eleven",Spell_digit.convert(111)
+   assert_equal "twohundredand twelve",Spell_digit.convert(212)
+   assert_equal "ninehundred ninety nine",Spell_digit.convert(999)
+   assert_equal "twothousand twohundrend twenty",Spell_digit.convert(2220)
+   assert_equal "threethousand threehundrend eleven",Spell_digit.convert(3311)
+ 
+   
    
   end
 end
